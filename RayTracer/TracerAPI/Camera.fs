bc7df30902c9d0386f5a314bd8295f1109d03977
Lillie <aedl@itu.dk>
2016-03-31 12:09:14 +0200

    function for shooting ray through image

--- RayTracer/TracerAPI/Camera.fs
+++ RayTracer/TracerAPI/Camera.fs
@@ -12,5 +12,18 @@ type unitRes =
 type camera = 
     | C of point * point * vector * float * unitRes * resolution
 
+type result =           
+    | Res of int * int * System.Drawing.Color //TODO make own color implementation.
+
 let mkCamera (pos : point) (look : point) (up : vector) (zoom : float) (width : float)
     (height : float) (pwidth : int) (pheight : int) : camera = C(pos, look, up, zoom, UR(width, height), R(pwidth, pheight))
+
+let shootRays (C(p, q, up, z, (UR(w, h)), (R(pw,ph)))) =
+    let l = Point.direction p q
+    let r = Vector.crossProduct up l |> Vector.normalise
+    let d = Vector.crossProduct r l |> Vector.normalise
+    let p' = Point.move p ( z * l)
+    let p' = Point.move p' ((h/2.0) * up)
+    let p' = Point.move p' ((-w/2.0) * r )
+    p'
+
--- RayTracer/TracerAPI/Camera.fsi
+++ RayTracer/TracerAPI/Camera.fsi
@@ -3,6 +3,12 @@
 type point = Point.Point
 type vector = Vector.Vector
 
+type resolution
+type unitRes
 type camera
+type result
 
-val mkCamera : point -> point -> vector -> float -> float -> float -> float -> float
+
+val mkCamera : point -> point -> vector -> float -> float -> float -> int -> int -> camera
+
+val shootRays : camera -> point
