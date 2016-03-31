1d50358e04752a7dd58d13922d3d12a9383b08d6
Emma Arfelt Kock <emmaarfelt@gmail.com>
2016-03-31 15:15:13 +0200

    Drawing method

--- RayTracer/TracerAPI/Drawing.fs
+++ RayTracer/TracerAPI/Drawing.fs
@@ -8,10 +8,15 @@ let height = 320
 
 let bmp = new Bitmap(width,height)
 
+type Result = {x:int; y:int; color:Color}
+
+let resultList = [{x = 3;y = 3;color = Color.Red},{x = 4;y = 4;color = Color.Black},{x = 5;y = 5;color = Color.Blue}]
+
 //Makes a picture by using list provided by the Camera
 let rec mkPicture list =
-    match list with 
+    match list with
     | []   -> []
-    | x::xs -> bmp.SetPixel(x) 
-               mkPicture xs 
+    | x::xs -> bmp.SetPixel(x)
+               mkPicture xs
+               
 bmp.Save("output.jpg")
