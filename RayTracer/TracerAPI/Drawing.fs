5b98f0773685984f0fd669dd6e4957feacce6d5e
akoc <akoc@itu.dk>
2016-03-31 13:05:06 +0200

    Drawing.fs added with MakePicture function

---
+++ RayTracer/TracerAPI/Drawing.fs
@@ -0,0 +1,17 @@
+﻿open TracerAPI
+open System
+open System.Drawing
+
+
+let width = 480
+let height = 320
+
+let bmp = new Bitmap(width,height)
+
+//Makes a picture by using list provided by the Camera
+let rec mkPicture list =
+    match list with 
+    | []   -> []
+    | x::xs -> bmp.SetPixel(x) 
+               mkPicture xs 
+bmp.Save("output.jpg")
