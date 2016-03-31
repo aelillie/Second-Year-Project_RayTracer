2e19785f8ddaf0fb0ad97eb48f82434606436175
Ramos <aara@itu.dk>
2016-03-31 13:47:52 +0200

    Colour and DrawAttributes

--- RayTracer/DrawAttributes/Colour.fs
+++ RayTracer/DrawAttributes/Colour.fs
@@ -1,2 +1,15 @@
-﻿module Colour
+﻿module colour
+
+
+type colour =
+  | C of float * float * float
+  override c.ToString() =
+    match v with
+      C(x,y,z) -> "["+x.ToString()+","+y.ToString()+","+z.ToString()+"]"
+
+
+let mkColour (r : float) (g : float) (b : float) : colour = C (r, g, b)
+let fromColor (c : System.Drawing.Color) : colour = 
+
+
 
