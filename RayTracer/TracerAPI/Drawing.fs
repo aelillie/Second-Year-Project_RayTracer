97d2f29d10a6b6066f07c06906e2ca4eb1b7d9e1
akoc <akoc@itu.dk>
2016-03-31 14:26:43 +0200

    Drawing works now.

--- RayTracer/TracerAPI/Drawing.fs
+++ RayTracer/TracerAPI/Drawing.fs
@@ -11,7 +11,7 @@ let bmp = new Bitmap(width,height)
 //Makes a picture by using list provided by the Camera
 let rec mkPicture list =
     match list with 
-    | []   -> []
+    | []    -> bmp
     | x::xs -> bmp.SetPixel(x) 
                mkPicture xs 
 bmp.Save("output.jpg")
