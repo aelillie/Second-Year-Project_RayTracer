a386bab90069a563c37b8ef73bd3d41b66cda548
Lillie <aedl@itu.dk>
2016-03-31 10:53:28 +0200

    init of camera implementation

---
+++ RayTracer/TracerAPI/Camera.fs
@@ -0,0 +1,16 @@
+﻿module Camera
+
+type point = Point.Point
+type vector = Vector.Vector
+
+type resolution = 
+    | R of int * int
+
+type unitRes =
+    | UR of float * float
+
+type camera = 
+    | C of point * point * vector * float * unitRes * resolution
+
+let mkCamera (pos : point) (look : point) (up : vector) (zoom : float) (width : float)
+    (height : float) (pwidth : int) (pheight : int) : camera = C(pos, look, up, zoom, UR(width, height), R(pwidth, pheight))
---
+++ RayTracer/TracerAPI/Camera.fsi
@@ -0,0 +1,8 @@
+﻿module Camera
+
+type point = Point.Point
+type vector = Vector.Vector
+
+type camera
+
+val mkCamera : point -> point -> vector -> float -> float -> float -> float -> float
