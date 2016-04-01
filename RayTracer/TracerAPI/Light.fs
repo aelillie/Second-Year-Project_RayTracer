5c825faca13b4557d587a6cec564c11d5c23df20
Lillie <aedl@itu.dk>
2016-04-01 15:48:17 +0200

    init of Light

---
+++ RayTracer/TracerAPI/Light.fs
@@ -0,0 +1,12 @@
+﻿module Light
+open Point
+open Material
+
+type Light =
+    | L of Point * Colour * float
+
+type AmbientLight =
+    | AL of Colour * float
+
+let mkLight (p : Point) (c : Colour) (i : float) : Light = failwith "mkLight not implemented"
+let mkAmbientLight (c : Colour) (i : float) : AmbientLight = failwith "mkAmbientLight not implemented"
---
+++ RayTracer/TracerAPI/Light.fsi
@@ -0,0 +1,9 @@
+﻿module Light
+open Point
+open Material
+
+type Light
+type AmbientLight
+
+val mkLight : position : Point -> colour : Colour -> intensity : float -> Light
+val mkAmbientLight : colour : Colour -> intensity : float -> AmbientLight
