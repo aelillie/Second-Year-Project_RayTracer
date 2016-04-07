b2884ca75899aa09e187888e5c45f830420203b0
Lillie <aedl@itu.dk>
2016-04-07 22:31:04 +0200

    simplify scale

--- RayTracer/TracerAPI/Transformation.fs
+++ RayTracer/TracerAPI/Transformation.fs
@@ -8,13 +8,19 @@ type Transformation =
 let idMatrix = Array2D.init<float> 4 4 
                 (fun row col -> if row = col then 1.0 else 0.0)
     
-let scale x y z = let m = Array2D.init<float> 4 4 
-                           (fun row col -> match row = col with
-                                           | true -> match row with
-                                                     | 0 -> x
-                                                     | 1 -> y
-                                                     | 2 -> z
-                                                     | 3 -> 1.0
-                                                     | _ -> failwith "Out of bounds"
-                                           | false -> 0.0)
-                  T(m, m)
+let makeInverse (m : float[,]) =
+    let a = m.[0, 3]*(-1.0)
+    let b = m.[1, 3]*(-1.0)
+    let c = m.[2, 3]*(-1.0)
+    Array2D.set m 0 3 a
+    Array2D.set m 1 3 b
+    Array2D.set m 2 3 c
+    m
+
+let scale x y z = 
+    let m = Array2D.zeroCreate<float> 4 4
+    Array2D.set m 0 0 x
+    Array2D.set m 1 1 y
+    Array2D.set m 2 2 z
+    Array2D.set m 3 3 1.0
+    T(m, makeInverse m)
