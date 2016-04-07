b1b84a9526520c04412fbd3756f722bc2be7b04c
Kevin Jensen <kejj@itu.dk>
2016-04-07 15:43:57 +0200

    Using testSuite now

--- RayTracer/Tests/Program.fs
+++ RayTracer/Tests/Program.fs
@@ -8,6 +8,7 @@ open Drawing
 open Light
 open System.Drawing
 open Point
+open TracerTest
 
 [<EntryPoint>]
 let main argv = 
@@ -15,14 +16,16 @@ let main argv =
 //    VectorTest.doTest()
 //    ExprParseTest.doTest()
 //    ExprToPolyTest.doTest()
-
-    let light = mkLight (mkPoint 0.0 2.0 4.0) (fromColor Color.White) 1.0 
+    TracerTest.doTest()  
+    (*let light = mkLight (mkPoint 0.0 2.0 4.0) (fromColor Color.White) 1.0 
     let ambientLight = mkAmbientLight (fromColor Color.White) 0.1 
     let camera = mkCamera (mkPoint 0.0 0.0 4.0) (mkPoint 0.0 0.0 0.0) (mkVector 0.0 1.0 0.0) 1.0 2.0 2.0 500 500 
     let sphere = mkSphere (mkPoint 0.0 0.0 0.0) 2.0 (mkMaterial (fromColor Color.Blue) 0.0)
     let scene = Scene.mkScene [sphere] [light] ambientLight camera 0
-    Scene.renderToFile scene |> ignore
+    Scene.renderToFile scene "fuck dat" |> ignore
+    *)
     System.Console.WriteLine "Saved image!"
+
     System.Console.WriteLine "Press any key to close..."
     System.Console.ReadKey() |> ignore
     0 // return an integer exit code
--- RayTracer/Tests/Scene.fs
+++ RayTracer/Tests/Scene.fs
@@ -7,6 +7,7 @@ open Tracer
 open Ray
 open Point
 open Vector
+open Drawing
 
 type Scene =
   | S of Shape list * Light list * AmbientLight * Camera * int
@@ -26,45 +27,39 @@ let renderToFile (S(shapes, lights, ambi, cam, n)) (filename:string) =
             let sr = Point.direction p' (Light.getPoint l)
             Ray.mkRay p' 1.0 sr
         
-        let lightIntensity (ray:Ray) (s:Shape) (l:Light) nV =
-         
-         match hit ray s with
-         |None    -> let i = (Light.calculateI nV (Ray.getD ray) (Light.getLightI l))
-                     i
-
-         |Some(t',_,_) -> let o = Camera.getPoint cam
-                          let tlight = Point.distance (Ray.getP ray) o |> Vector.magnitude  
-                          if t' > tlight 
-                          then 
-                            let i =(Light.calculateI nV (Ray.getD ray) (Light.getLightI l))
-                            i
-                          else
-                            0.0
+        let lightIntensity (r:Ray) (s:Shape) (l:Light) nV =
+             match hit ray s with
+             |None    -> let i = (Light.calculateI nV (Ray.getD r) (Light.getLightI l))
+                         i
+             |Some(t',_,_) -> let o = Camera.getPoint cam
+                              let tlight = Point.distance (Ray.getP r) o |> Vector.magnitude  
+                              if t' < tlight 
+                              then 
+                                let i =(Light.calculateI nV (Ray.getD r) (Light.getLightI l))
+                                i
+                              else
+                                0.0
 
         let sort = function
-         |None -> [] 
-         |Some(t,nV,c) -> [(t,nV,c)]
+            |None -> [] 
+            |Some(t,nV,c) -> [(t,nV,c)]
 
 
         let intersects = List.collect (fun x -> sort x) intersects
 
         match intersects with 
-         | [] -> System.Drawing.Color.White
-         | _  -> let (t,nV,c) = List.minBy (fun (t,_,_) -> t ) intersects
-                 let nV' = if (Ray.getD ray) * nV > 0.0 then (-1.0 * nV) else nV
-                 let i = Light.getAmbientI ambi
-                 let p = Point.move (Ray.getP ray) (Vector.multScalar (Ray.getD ray) t)
-                 let srays = List.map (fun x -> (x, calculateShadowRay p nV' x 0.0001)) lights
-                 let il = List.map2 (fun (l, r) s -> lightIntensity r s l nV') srays shapes
-                 let i' = List.fold (fun acc x -> x + acc) 0.0 il
-                 Colour.toColor (Colour.scaleColour c (i+i'))
-                   
-                 
-
-
+            | [] -> System.Drawing.Color.White
+            | _  -> let (t,nV,c) = List.minBy (fun (t,_,_) -> t ) intersects
+                    let nV' = if (Ray.getD ray) * nV > 0.0 then (-1.0 * nV) else nV
+                    let i = Light.getAmbientI ambi
+                    let p = Point.move (Ray.getP ray) (Vector.multScalar (Ray.getD ray) t)
+                    let srays = List.map (fun x -> (x, calculateShadowRay p nV' x 0.0001)) lights
+                    let il = List.map2 (fun (l, r) s -> lightIntensity r s l nV') srays shapes
+                    let i' = List.fold (fun acc x -> acc + x) 0.0 il
+                    Colour.toColor (Colour.scaleColour c (i+i'))
     let pixelplane = List.map (fun (r, (x,y)) ->(x,y, castRay r n)) rays
 
-    Drawing.mkPicture pixelplane 500 500 |> ignore
+    Drawing.mkPicture pixelplane 500 500 filename 
 
 
 
