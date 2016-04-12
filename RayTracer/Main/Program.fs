52b10c155a54e49333d94980ac957e78f8ce8b7a
Lillie <aedl@itu.dk>
2016-04-12 11:24:19 +0200

    Refactor main entry point

---
+++ RayTracer/Main/Program.fs
@@ -0,0 +1,10 @@
+﻿//Entry point for running the program
+
+[<EntryPoint>]
+let main argv = 
+
+    //Run code here
+
+    System.Console.WriteLine "Press any key to close..."
+    System.Console.ReadKey() |> ignore
+    0 // return an integer exit code
