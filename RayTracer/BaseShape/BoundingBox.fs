ad35649d05f7510d06d8fb9fd8589d8980a32472
Emma Arfelt Kock <emmaarfelt@gmail.com>
2016-04-14 13:44:57 +0200

    fixed boundingbox

--- RayTracer/BaseShape/BoundingBox.fs
+++ RayTracer/BaseShape/BoundingBox.fs
@@ -1,6 +1,6 @@
 ﻿module BoundingBox
 open System
-open Point g
+open Point 
 open Shape
 
 
@@ -9,27 +9,30 @@ type BoundingBox =
 
 let mkBoundingBox l h = B(l,h)
 
+//Epsilon
 let epsilon = 0.00001
 
-let calcL s =
+//Match on shape to calculate max and min values for boundingbox 
+let calc s =
     match s with
     | S(p,f,m) -> 
         let lx = (Point.getX p) - f - epsilon
         let ly = (Point.getY p) - f - epsilon
         let lz = (Point.getZ p) - f - epsilon
-        let l = P(lx,ly,lz) 
+        let l = mkPoint lx ly lz
 
         let hx = (Point.getX p) + f + epsilon
         let hy = (Point.getY p) + f + epsilon
         let hz = (Point.getZ p) + f + epsilon 
-        let h = P(hx,hy,hz)
-        B(l,h)
+        let h = mkPoint hx hy hz
+        mkBoundingBox l h
+
    (* Triangle bounding box
     | S(a,b,c,m) -> 
         let xlist = [(Point.getX a);(Point.getX b);(Point.getX c)]
         let ylist = [(Point.getY a);(Point.getY b);(Point.getY c)]
         let zlist = [(Point.getZ a);(Point.getZ b);(Point.getY c)]
 
-        let l = P((List.min xlist), (List.min ylist), (List.min zlist))
-        let h = P((List.max xlist), (List.max ylist), (List.max zlist))
+        let l = P(((List.min xlist) + epsilon), ((List.min ylist)+epsilon), ((List.min zlist)+epsilon))
+        let h = P(((List.max xlist) + epsilon), ((List.max ylist)+epsilon), ((List.max zlist)+epsilon))
         B(l,h) *)
