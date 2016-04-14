884dd44bf95a9b2cc1fc2095b72fcf1bc5f51562
Emma Arfelt Kock <emmaarfelt@gmail.com>
2016-04-14 14:37:09 +0200

    new function in boundingbox

--- RayTracer/BaseShape/BoundingBox.fsi
+++ RayTracer/BaseShape/BoundingBox.fsi
@@ -5,3 +5,6 @@ open Shape
 type BoundingBox 
 
 val mkBoundingBox : Point -> Point -> BoundingBox 
+val calc : Shape -> BoundingBox
+val getL : BoundingBox -> Point
+val getH : BoundingBox -> Point 
--- RayTracer/Tests/BoundingBoxTest.fs
+++ RayTracer/Tests/BoundingBoxTest.fs
@@ -1,6 +1,14 @@
-﻿
-
-namespace Tests
+﻿module Tests
 open System
+open Shape
+open BoundingBox
+open Point 
+open System.Drawing
 
-type BoundingBoxTest() = 
+
+let s = mkSphere (mkPoint 2.0 2.0 -1.2) 1.0 (Material.mkMaterial (Colour.fromColor Color.Green) 0.5)
+let bs = calc s
+
+
+
+
