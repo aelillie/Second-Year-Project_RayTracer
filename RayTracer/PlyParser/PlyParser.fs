d8be5a74bd092112147298ae28b1db417e4ca314
Kevin <kejj@itu.dk>
2016-04-17 22:53:41 +0200

    Improved Parser, can now also parse ply files with normal vectors and texture coord correctly as well as Element information

--- RayTracer/PlyParser/PlyParser.fs
+++ RayTracer/PlyParser/PlyParser.fs
@@ -7,12 +7,25 @@ open System.IO
 type UserState = unit
 type Ply = 
          | Vertex of float * float * float
+         | Vertex2 of float * float * float * float * float
+         | Vertex3 of float * float * float * float * float * float * float * float
          | Face of int list
          | Comment of string
-         | DummyData
+         | DummyData of string
          | Element of string * int
          | Endheader 
 
+
+let print p writer = 
+      match p with
+         | Vertex (x,y,z) -> fprintf writer "Vertex %f %f %f \n" x y z
+         | Face (x) ->       fprintf writer "Face %O \n" x
+         | Comment x -> fprintf writer "Comment %s \n" x
+         | DummyData x -> fprintf writer "DummyData %s \n" x
+         | Element (e, n) -> fprintf writer "Element %s %d \n" e n
+         | Endheader -> fprintf writer "end_header \n"
+         | Vertex2 (x,y,z,u,v) -> fprintf writer "Vertex %f %f %f %f %f \n" x y z u v
+         | Vertex3 (x,y,z,nx,ny,nz,u,v) -> fprintf writer "Vertex %f %f %f %f %f %f %f %f \n" x y z nx ny nz u v
 
 type Parser<'t> = Parser<'t, UserState>    
 
@@ -23,30 +36,50 @@ let readLines (filePath:string) = seq {
         yield sr.ReadLine ()
 }
 
+let mkVertex n (xs:float list) =
+    match n with
+    |3 -> Vertex((xs.Item 0),xs.Item 1,xs.Item 2)  
+    |5 -> Vertex2((xs.Item 0),xs.Item 1,xs.Item 2,(xs.Item 3),xs.Item 4)  
+    |8 -> Vertex3((xs.Item 0),xs.Item 1,xs.Item 2,(xs.Item 3),xs.Item 4,(xs.Item 5),xs.Item 6,xs.Item 7)  
+    |_ -> failwith "Encounted an errornous number of values when creating a vertex of %d, %O" n xs
+    
+//Helper parsers
+let pSingleSpace: Parser<_> = satisfy (fun c ->  System.Char.IsWhiteSpace c) 
+let pElementName: Parser<_> = satisfy (fun c ->  System.Char.IsLetterOrDigit c)
+let pList n : Parser<_> = parray n (pint32 .>> spaces)
+
 
 // Set up parsers for each type  
 let pComment: Parser<_> =  pstring "comment" >>. restOfLine true |>> (fun x -> Comment x)
 let pEndHeader: Parser<_> = pstring "end_header" .>> skipRestOfLine true |>> (fun _ -> Endheader)
-let pVertex: Parser<_> = pfloat .>>. (spaces >>. pfloat) .>>. (spaces >>. pfloat) |>> (fun ((x,y),z) -> Vertex (x,y,z))
-let pFace: Parser<_> = pint32 >>? spaces1 >>. many1 (pint32 .>> spaces1) |>> (fun x -> Face (x))
-let pDummyData: Parser<_> = skipRestOfLine true |>> (fun _ -> DummyData)
-let pElement: Parser<_> = pstring "element" >>. spaces >>. (many1Chars anyChar) .>> spaces .>>.  pint32 |>> (fun (x,f) -> Element (x,f))
+let pVertex: Parser<_> = many1 (pfloat .>> pSingleSpace) .>> spaces |>> (fun x -> mkVertex x.Length x)
+//let pVertex: Parser<_> = pfloat .>>. (spaces >>. pfloat) .>>. (spaces >>. pfloat) |>> (fun ((x,y),z) -> Vertex (x,y,z))
+//let pFace: Parser<_> = pint32 .>>? spaces1 >>= pList .>> spaces |>> (fun x -> mkFace x)
+let pFace: Parser<_> = pint32 .>>? spaces1 >>= pList |>> (fun x -> Face (Array.toList x))
+//let pFace: Parser<_> = pint32 .>>? spaces1 .>>. many1 (pint32 .>> spaces1) |>> (fun (y,x) -> Face (x))
+let pDummyData: Parser<_> = skipRestOfLine true |>> (fun x -> DummyData "R")
+let pElement: Parser<_> = pstring "element" >>. spaces >>. (many1Chars pElementName) .>> spaces .>>.  pint32 |>> (fun (x,f) -> Element (x,f))
+
 
 // Create a parser which can choose from each of them
-let pPly: Parser<_> = (pComment <|> pEndHeader) <|> (attempt pFace <|> pVertex) <|> (pElement <|> pDummyData)
-
+let pPly2: Parser<_> = choice [pComment 
+                               pElement 
+                               pEndHeader 
+                               attempt pFace 
+                               pVertex 
+                               pDummyData]
 // Helper method returning the result (ply type) or DummyData if failure
 let parse p str = 
     match run p str with
      |Success(result,_,_) -> result
-     |Failure(er,_,_) -> DummyData
+     |Failure(er,_,_) -> DummyData er
           
 
 let parsePly filePath =
 
     let lines = readLines filePath // read lines
     
-    Seq.map (fun x -> parse pPly x) lines |> Seq.toList // parse all lines and map them
+    Seq.map (fun x -> parse pPly2 x) lines |> Seq.toList // parse all lines and map them
     
     
 
--- RayTracer/Tests/Program.fs
+++ RayTracer/Tests/Program.fs
@@ -18,10 +18,23 @@ let main argv =
     VectorTest.doTest()
     ExprParseTest.doTest()
     ExprToPolyTest.doTest()*)
-    TracerTest.doTest()  
+    //TracerTest.doTest()  
+
+    let writer = new StreamWriter (@"C:\Users\i5-4670K\Documents\test.rtf", false, System.Text.Encoding.UTF8,512)
+
+    writer.AutoFlush <- true
 
 
-    
+    let filepath = @"C:\Users\i5-4670K\Documents\urn2.ply.txt"
+
+
+    let ply = PlyParse.parsePly filepath
+
+
+    List.iter (fun x -> PlyParse.print x writer) ply
+
+
+        
     System.Console.WriteLine "Press any key to close..."
     System.Console.ReadKey() |> ignore
     0 // return an integer exit code
