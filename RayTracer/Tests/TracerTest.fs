bea3a7eee60076952c8faced7dea5af1169729e2
Lillie <aedl@itu.dk>
2016-04-19 14:36:20 +0200

    release picture

--- RayTracer/Tests/TracerTest.fs
+++ RayTracer/Tests/TracerTest.fs
@@ -21,7 +21,7 @@ open System.Drawing
         | None -> ()
       let toScreen = false 
       let renderSphere toScreen =
-        let light = mkLight (mkPoint 0.0 3.0 0.0) (Colour.fromColor Color.White) 1.0
+        let light = mkLight (mkPoint 3.0 3.0 0.0) (Colour.fromColor Color.White) 1.0
         let ambientLight = mkAmbientLight (Colour.fromColor Color.White) 0.1
         let camera = mkCamera (mkPoint 0.0 0.0 4.0) (mkPoint 0.0 0.0 0.0) (mkVector 0.0 1.0 0.0) 1.0 2.0 2.0 500 500
         let plane = mkPlane(mkPoint 0.0 -10.0 0.0) (mkVector 0.0 -1.0 -0.5) (Material.mkMaterial (Colour.mkColour 0.35 0.24 0.67) 0.0)
@@ -30,9 +30,9 @@ open System.Drawing
         let sphere2 = mkSphere (mkPoint 0.0 0.0 0.0) 1.0 (Material.mkMaterial (Colour.fromColor Color.Purple) 0.2)
         let hc = mkHollowCylinder (mkPoint 0.0 0.0 0.0) 1.0 2.0 (Material.mkMaterial (Colour.fromColor Color.Blue) 0.0)
 
-        let tsphere = transform sphere (mergeTransformations [scale 2.0 2.0 2.0;sheareYX 1.0;translate 1.0 2.5 -2.0;mirrorZ])
-        let tsphere1 = transform sphere1 (mergeTransformations [scale 0.5 0.5 0.5;sheareXY 1.0;translate -1.0 1.5 -2.0])
-        let tsphere2 = transform sphere2 (mergeTransformations [scale 1.0 1.0 1.0;sheareXZ 1.0;translate 2.0 -1.0 -1.0])
+        let tsphere = transform sphere (mergeTransformations [sheareYX 1.0;translate 1.0 2.5 -2.0])
+        let tsphere1 = transform sphere1 (mergeTransformations [scale 1.5 1.5 1.5;translate -1.0 1.5 -2.0])
+        let tsphere2 = transform sphere2 (mergeTransformations [rotateZ System.Math.PI;translate -2.0 1.0 1.0])
 
         let scene = Scene.mkScene [plane; tsphere; tsphere1; tsphere2] [light] ambientLight camera 2
         if toScreen then
