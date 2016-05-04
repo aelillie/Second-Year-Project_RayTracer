c8c5fe136eff414d7ba17b570c0d65b6e87cffd1
Anders Edelbo Lillie <aedl@itu.dk>
2016-05-04 21:16:13 +0200

    setup main for entry point

---
+++ RayTracer/Main/Utils.fs
@@ -0,0 +1,28 @@
+﻿namespace Utilities
+
+open System.IO
+open Scene
+open System
+
+module Util =
+
+    let degrees_to_radians (d : float) = d * Math.PI / 180.0
+    
+    
+    let private source_path = "../../.."
+    let private ply_path = source_path + "/ply"
+    let private result_path = source_path + "/result"
+    
+    let render s (toFile : (string*string) option) =
+      match toFile with
+      | Some (f, fn) -> 
+        let path = if f = "" then result_path else result_path + "/" + f
+        Directory.CreateDirectory path |> ignore
+        renderToFile s (path + "/" + fn)
+      | None -> renderToScreen s
+    
+    let render' (s : Scene) (toFile : (string*string)) (toScreen : bool) = 
+      if toScreen
+      then render s None
+      else render s (Some toFile)
+    
