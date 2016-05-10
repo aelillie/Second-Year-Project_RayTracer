05b726a9143769cb0971338e1db0adc27fc6ad33
Kevin Jensen <kejj@itu.dk>
2016-05-10 17:36:42 +0200

    Changed epsilon to a lower value.

--- RayTracer/Main/Program.fs
+++ RayTracer/Main/Program.fs
@@ -21,23 +21,24 @@ open System.Drawing
 let main argv =
 
     let folder = "shapes"
-
+     
     let render toScreen =
+        (******Helper******)
+        let mkColourTexture c r = mkMatTexture (mkMaterial (fromColor c) r)
+
         (*******Light******)
-        let light = mkLight (mkPoint 0.0 3.0 5.0) (fromColor Color.White) 1.0 in
+        let light = mkLight (mkPoint 0.0 0.0 4.0) (fromColor Color.White) 1.0 in
         let ambientLight = mkAmbientLight (fromColor Color.White) 0.1 in
         (*******Camera******)
         let camera = mkCamera (mkPoint 0.0 0.0 4.0) (mkPoint 0.0 0.0 0.0) (mkVector 0.0 1.0 0.0) 1.0 2.0 2.0 500 500 in
         (*******Shapes******)
-        let sphere = mkSphere (mkPoint 0.0 0.0 0.0) 1.0 (mkMatTexture (mkMaterial(Colour.fromColor Color.Blue) 0.0)) in
-        let plane = mkPlane (Texture.loadTexture "C:\Users\Amalie\Documents\checkerboard2.jpg")
-        let unitBox p1 p2 t = mkBox p1 p2 t t t t t t
-        let box = unitBox (mkPoint -1.0 -1.0 -1.0) (mkPoint 1.0 1.0 1.0) (Texture.loadTexture "C:\Users\Amalie\Documents\checkerboard2.jpg")
-        let disc = mkDisc (mkPoint 0.0 0.0 0.0) 3.4 (Texture.loadTexture "C:\Users\Amalie\Documents\checkerboard2.jpg")
-        let HC = mkHollowCylinder (mkPoint 0.0 0.0 0.0) 2.0 4.0 (Texture.loadTexture "C:\Users\Amalie\Documents\sobillede.jpg")
-        let SC = mkSolidCylinder (mkPoint 0.0 0.0 0.0) 2.0 4.0 (Texture.loadTexture "C:\Users\Amalie\Documents\sobillede.jpg")(Texture.loadTexture "C:\Users\Amalie\Documents\sobillede.jpg")(Texture.loadTexture "C:\Users\Amalie\Documents\sobillede.jpg")
-        let SC' = transform SC (rotateY (System.Math.PI))
-
+        let SC = mkSolidCylinder (mkPoint 0.0 0.5 0.0) 1.0 2.0 (mkColourTexture Color.Red 0.0) (mkColourTexture Color.Red 0.0) (mkColourTexture Color.Red 0.0)
+        let Box = mkBox (mkPoint -1.0 0.0 0.0) (mkPoint 1.0 1.0 1.0) (mkColourTexture Color.Yellow 0.0) (mkColourTexture Color.Yellow 0.0) (mkColourTexture Color.Yellow 0.0) (mkColourTexture Color.Yellow 0.0) (mkColourTexture Color.Yellow 0.0) (mkColourTexture Color.Yellow 0.0) 
+        let Sphere = mkSphere (mkPoint 0.0 0.5 0.0) 1.0 (mkColourTexture Color.Blue 0.0)
+        let Sphere2 = mkSphere (mkPoint 0.0 0.5 2.0) 1.0 (mkColourTexture Color.Blue 0.0)
+        let u = union Box Sphere
+        let SC' = subtraction SC u
+        let SC' = transform SC' (rotateY (System.Math.PI/4.0))
         (*******Scene******)
         let scene = mkScene [SC'] [light] ambientLight camera 0 in
         if toScreen then
