5b593bf6ab14d3b9499163cb822e89e81345a4bd
Lillie <aedl@itu.dk>
2016-05-22 18:17:11 +0200

    test case

--- RayTracer/Main/Program.fs
+++ RayTracer/Main/Program.fs
@@ -30,23 +30,23 @@ let main argv =
         let mkColourTexture c r = mkMatTexture (mkMaterial (fromColor c) r)
 
         (*******Light******)
-        let light = mkLight (mkPoint 0.0 0.0 4.0) (fromColor Color.White) 1.0
+        let light = mkLight (mkPoint 1.0 0.0 10.0) (fromColor Color.White) 1.0
         let light1 = mkLight (mkPoint 0.0 4.0 0.0) (fromColor Color.White) 1.0
         let ambientLight = mkAmbientLight (fromColor Color.White) 0.1 in
         (*******Camera******)
-        let camera = mkCamera (mkPoint 0.0 0.0 4.0) (mkPoint 0.0 0.0 0.0) (mkVector 0.0 1.0 0.0) 1.0 2.0 2.0 500 500 in
+        let camera = mkCamera (mkPoint 0.0 0.0 10.0) (mkPoint 0.0 0.0 0.0) (mkVector 0.0 1.0 0.0) 1.0 2.0 2.0 1000 1000 in
         (*******Shapes******)
         let s = System.Diagnostics.Stopwatch.StartNew()
-        let ply = mkPLY "../../../ply/apple.ply" false
+        let ply = mkPLY "../../../ply/ant.ply" true
         s.Stop() ; printf "Ply parsed in %f seconds\n" s.Elapsed.TotalSeconds
-        let tex = (mkTextureFromFile (fun x y -> (y,x)) "../../../textures/bunny.png")
+        //let tex = (mkTextureFromFile (fun x y -> (y,x)) "../../../textures/bunny.png")
         let mat c = mkMatTexture (mkMaterial (fromColor c) 0.0)
-        let apple = transform (mkShape ply (mat Color.Green)) (mergeTransformations [rotateX (pi/4.0);scale 30.0 30.0 30.0])
-        let plane = transform (mkPlane (mat Color.LightGray)) (rotateX (pi/2.0)) 
+        let ant = transform (mkShape ply (mat Color.Gold)) (scale 0.1 0.1 0.1)
+        //let plane = transform (mkPlane (mat Color.LightGray)) (rotateX (pi/2.0)) 
 
         (*******Scene******)
 
-        let scene = mkScene [apple] [light;light1] ambientLight camera 3
+        let scene = mkScene [ant] [light] ambientLight camera 3
         if toScreen then
           Util.render scene None
         else
