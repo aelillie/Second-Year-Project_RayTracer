e5221752de4a329a1adafd65793490ffb2e68e3e
Anders Edelbo Lillie <aedl@itu.dk>
2016-05-23 16:30:27 +0200

    updated test suite

--- RayTracer/Tests/Shapes.fs
+++ RayTracer/Tests/Shapes.fs
@@ -32,6 +32,19 @@ module Shapes =
     else
       Util.render scene (Some (folder, "renderSphere.png"))
 
+  let renderReflectiveSpheres toScreen =
+    let light = mkLight (mkPoint 0.0 0.0 2.0) (mkColour 1. 1. 1.) 1.0
+    let ambientLight = mkAmbientLight (mkColour 1. 1. 1.) 0.2
+    let camera = mkCamera (mkPoint 0.0 0.0 2.0) (mkPoint 0.0 0.0 0.0) (mkVector 0.0 1.0 0.0) 1.0 2.0 2.0 500 500
+    let sphere = mkSphere (mkPoint -0.8 0.0 0.0) 0.7 (mkMatTexture (mkMaterial (mkColour 0. 0. 1.) 0.7))
+    let sphere2 = mkSphere (mkPoint 0.8 0.0 0.0) 0.7 (mkMatTexture (mkMaterial (mkColour 1. 0. 0.) 0.7))
+    //let plane = mkPlane (mkPoint 0.0 0.0 0.0) (mkVector 1. 1. 1.) (mkMatTexture (mkMaterial (mkColour 0. 1. 0.) 0.8))
+    let scene = mkScene [sphere;sphere2] [light] ambientLight camera 4
+    if toScreen then
+      Util.render scene None
+    else
+      Util.render scene (Some (folder, "renderReflectiveSpheres.png"))
+
   let renderHollowCylinder toScreen =
     let light = mkLight (mkPoint 2.0 3.0 4.0) (fromColor Color.White) 1.0 in
     let ambientLight = mkAmbientLight (fromColor Color.White) 0.1 in
@@ -70,6 +83,7 @@ module Shapes =
 
   let render toScreen =
     renderSphere toScreen;
+    renderReflectiveSpheres toScreen;
     renderHollowCylinder toScreen;
     renderSolidCylinder toScreen;
     renderInsideSphere toScreen
