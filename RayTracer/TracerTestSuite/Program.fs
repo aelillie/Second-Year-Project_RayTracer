e0469494f28ebd1fb5e6f782b6608a4ea2d43c23
Emil Hørning <emil_chr@msn.com>
2016-05-26 11:21:06 +0200

    Testmaster

---
+++ RayTracer/TracerTestSuite/Program.fs
@@ -0,0 +1,18 @@
+﻿open TracerTestSuite
+
+let renderAll toScreen =
+  Shapes.render toScreen
+  AffineTransformations.render toScreen
+  ImplicitSurfaces.render toScreen
+  Meshes.render toScreen
+  Texture.render toScreen
+  Light.render toScreen
+  CSG.render toScreen
+
+
+[<EntryPoint>]
+let main argv =
+    Util.init();
+    renderAll false;
+    Util.finalize();
+    0 // return an integer exit code
